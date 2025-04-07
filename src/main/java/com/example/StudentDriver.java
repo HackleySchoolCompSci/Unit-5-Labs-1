77135f5de06a99a6d5f0228b3856f5726cd93297
phoebecd <87622936+phoebecd@users.noreply.github.com>
2025-04-06 23:19:55 -0400

    Update StudentDriver.java

--- src/main/java/com/example/StudentDriver.java
+++ src/main/java/com/example/StudentDriver.java
@@ -4,16 +4,17 @@ import java.util.Scanner;
 public class StudentDriver {
     public static void main(String[] args)
     {
+	Scanner scan = new Scanner(System.in); // Create a single Scanner instance
+
 	Student student1 = new Student("Mary");
 	Student student2 = new Student("Mike");
 
-	student1.inputGrades();
+	student1.inputGrades(scan);
 	System.out.println (student1.getName() + "'s current average is " 
     + student1.getAverage() + "%.");
 
-	student2.inputGrades();
-	System.out.println ("The average for " + student2.getName() +
-			    " is " + student2.getAverage() + "%.");
+	student2.inputGrades(scan);
+	System.out.println ("The average for " + student2.getName() +	   " is " + student2.getAverage() + "%.");
 	
 
 	System.out.println ("Student 1: " + student1);
